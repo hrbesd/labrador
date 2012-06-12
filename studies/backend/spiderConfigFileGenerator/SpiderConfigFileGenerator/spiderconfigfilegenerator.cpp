53b06fc7f9a2b8bf6616c9e7814a80df04e4b269
hailong <longlongh4@gmail.com>
2012-06-12 20:55:09 +0800

    add one xml example

--- studies/backend/spiderConfigFileGenerator/SpiderConfigFileGenerator/spiderconfigfilegenerator.cpp
+++ studies/backend/spiderConfigFileGenerator/SpiderConfigFileGenerator/spiderconfigfilegenerator.cpp
@@ -8,6 +8,15 @@ SpiderConfigFileGenerator::SpiderConfigFileGenerator(QObject *parent) :
     websiteItem.url = "http://www.harbin.gov.cn/";
     websiteItem.regExp = "";
     websiteItem.websiteName = "chinese harbin";
+    Node nodeItem;
+    nodeItem.url = "http://www.harbin.gov.cn/zwxxgk/zfgg/zfgg2012.htm";
+    nodeItem.regExp = "<a .*href=\"([^\"]*)\" class=\"bgc1\" onmouseover=\"this.className=\'bgc2\'\" onmouseout=\"this.className=\'bgc1\'";
+    Node childNodeItem;
+    childNodeItem.nextPageRegExp="<a href=\"([^\"]*)\" class=\"Next\">下页";
+    childNodeItem.regExp = "<a class=\"f3348\" href=\"([^\"]*)\"";
+    childNodeItem.titleRegExp = "<a class=\"f3348\".*title=\"([^\"]*)\"";
+    nodeItem.nodeList.append(childNodeItem);
+    websiteItem.nodeList.append(nodeItem);
     YZXmlWriter::writeWebsiteItemToXml(websiteItem,"config.xml");
     qDebug()<<"finish";
 }
