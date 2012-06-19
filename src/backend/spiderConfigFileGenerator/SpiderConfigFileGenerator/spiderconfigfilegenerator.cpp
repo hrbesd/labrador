7ce648491c88ef2cd3637b2efce6f323180f301b
hailong <longlongh4@gmail.com>
2012-06-19 09:59:57 +0800

    add bumendongtai rule for spider

--- src/backend/spiderConfigFileGenerator/SpiderConfigFileGenerator/spiderconfigfilegenerator.cpp
+++ src/backend/spiderConfigFileGenerator/SpiderConfigFileGenerator/spiderconfigfilegenerator.cpp
@@ -9,7 +9,10 @@ SpiderConfigFileGenerator::SpiderConfigFileGenerator(QObject *parent) :
     websiteItem.info = "zhong guo haerbin";
     websiteItem.node.url = "http://www.harbin.gov.cn/";
     websiteItem.node.name = "chinese harbin";
-
+    /****************************************
+     *公告页面规则
+     *
+     ****************************************/
     Rule* gongGaoRule = new Rule;  //公告总页面
     Rule* gongGaoChildRule = new Rule;  //公告年份分页面
     Rule* gongGaoArticleRule = new Rule;    //公告正文页面
@@ -29,7 +32,32 @@ SpiderConfigFileGenerator::SpiderConfigFileGenerator(QObject *parent) :
     gongGaoArticleRule->nameRegExp = "<a class=\"f3348\".*title=\"([^\"]*)\"";
     gongGaoChildRule->childRule = gongGaoArticleRule;
 
+    /*************************************
+     *部门动态规则
+     *
+     ************************************/
+    Rule* buMenDongTaiRule = new Rule;  //公告总页面
+    Rule* buMenDongTaiChildRule = new Rule;  //公告年份分页面
+    Rule* buMenDongTaiArticleRule = new Rule;    //公告正文页面
+
+    Node buMenDongTaiNodeItem;
+    buMenDongTaiNodeItem.name = "bumendongtai";
+    buMenDongTaiNodeItem.url = "http://www.harbin.gov.cn/zwxxgk/bmdt.htm";
+    buMenDongTaiNodeItem.ruleList.append(buMenDongTaiChildRule);
+
+    buMenDongTaiRule->nodeList.append(buMenDongTaiNodeItem);
+
+    buMenDongTaiChildRule->urlRegExp = "<a .*href=\"([^\"]*)\" class=\"hei14\">";
+    buMenDongTaiChildRule->nameRegExp = "<FONT class=\"hei12\">(.*)</font>";
+
+    buMenDongTaiArticleRule->nextPageRegExp="<a href=\"([^\"]*)\" class=\"Next\">下页";
+    buMenDongTaiArticleRule->urlRegExp = " <a class=\"f3665\" href=\"([^\"]*)\"";
+    buMenDongTaiArticleRule->nameRegExp = "class=\"f3665\" title=\"([^\"]*)\"";
+    buMenDongTaiChildRule->childRule = buMenDongTaiArticleRule;
+
+
     websiteItem.node.ruleList.append(gongGaoRule);
+    websiteItem.node.ruleList.append(buMenDongTaiRule);
     YZXmlWriter::writeWebsiteItemToXml(websiteItem,"config.xml");
     qDebug()<<"finish";
 }
