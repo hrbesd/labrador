892130ab5eea2d4d4f24e3725f7d463564aa53d8
zhf@ubuntu <hf@zhanghongfeng.com>
2012-06-28 17:49:44 +0800

    to my mac

--- src/deployment/admin/console/create.sh
+++ src/deployment/admin/console/create.sh
@@ -1,5 +1,11 @@
+source `basedir $0`/utils/common.sh
+
 usage()
 {
-	
+	echo "$0: <id> <host>" >&2
+	exit 1	
 }
-test -n "$*" && usage
+
+test -n "$1" || test -n "$2" && usage
+
+cp -r $LABRADOR_TEMPLETS $LABRADOR_SITES/$1
