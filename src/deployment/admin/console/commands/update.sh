cafcd98a78d8b8c08085544dd5ea6dcfa99ddb77
zhf <hf@zhanghongfeng.com>
2012-06-29 02:41:57 +0800

    update from mbp

--- src/deployment/admin/console/commands/update.sh
+++ src/deployment/admin/console/commands/update.sh
@@ -1,6 +1,18 @@
 if test -z "$*"; then
-	echo "Error: At least one site must be specified." >&2
-	exit 1
+	SITE_ROOT=`get_siteroot`
+	if test -d "$SITE_ROOT"; then
+		echo "Error: At least one site must be specified." >&2
+		exit 1
+	else
+		`grep 'ID=' $SITE_ROOT/config/site.conf`
+		$SCHEDULER $ID
+	fi
+else
+	if test "$1" == "all"; then
+		$SCHEDULER_PATH
+	else
+		$SCHEDULER_PATH $*
+	fi
 fi
 
-$SCHEDULER_PATH $*
+
