2bb4022ddf4e006f9ae3f1a20f13a26ae5423cbe
zhf <hf@zhanghongfeng.com>
2012-06-28 22:51:58 +0800

    re-structure console

--- src/deployment/admin/console/lab.sh
+++ src/deployment/admin/console/lab.sh
@@ -1,9 +1,9 @@
 DIR=`dirname $0`
-DEFAULT_COMMAND="show"
+DEFAULT_COMMAND="stat"
 
 function usage
 {
-	printf "Usage:\n \t ec <command> [<arguments>]\n \t lab -e <command>\n \t ec -l\n"
+	printf "Usage:\n \t lab <command> [<arguments>]\n \t lab -e <command>\n \t lab -l\n"
 	local exit_status=$1
 	test -z "$exit_status" && exit_status=1 
 	exit $exit_status
@@ -32,7 +32,7 @@ shift
 [ "$COMMAND" == "help" ] && usage 0
 test -z "$COMMAND" && COMMAND=$DEFAULT_COMMAND
 
-COMMAND_SCRIPT="$DIR/$COMMAND.sh"
+COMMAND_SCRIPT="$DIR/commands/$COMMAND.sh"
 
 if [[ "$MODE" == "EXEC" || "$MODE" == "EDIT" ]]; then
 	if test ! -f $COMMAND_SCRIPT; then
@@ -43,7 +43,7 @@ fi
 
 case $MODE in
 	"EDIT") $EDITOR $COMMAND_SCRIPT ;;
-	"LIST") for i in `ls $DIR/*.sh`; do printf "`basename $i .sh`\n"; done ;;
+	"LIST") for i in `ls $DIR/commands/*.sh`; do printf "`basename $i .sh`\n"; done ;;
 	"EXEC") $COMMAND_SCRIPT $* ;;
 	?)		usage ;;
 esac
