b7632d8f4582a120ac88e5c73770308d650d9392
zhf@ubuntu <hf@zhanghongfeng.com>
2012-06-29 14:20:31 +0800

    add parameters

--- src/deployment/admin/scheduler/labrador-scheduler.sh
+++ src/deployment/admin/scheduler/labrador-scheduler.sh
@@ -17,6 +17,7 @@ for SITE in ${#SITES[@]};
 do
 	SITE_ROOT=$LABRADOR_SITES/$SITE
 	SITE_CONF=$SITE_ROOT/config/site.conf
+	CONF_DIR=$LABRADOR/etc
 
 	log "Begin processing site $SITE"
 	
@@ -24,27 +25,35 @@ do
 	$SPIDER_PATH --site-config=$SITE_CONF \
 		--worker-dir=$SITE_ROOT/workers/spider \
 		--shared-dir=$SITE_ROOT/workers/shared \
+		--config-dir=$CONF_DIR/spider \
 		--rule-dir=$SITE_ROOT/rules/spider \
 		--log-file=$SITE_ROOT/logs/spider.log
 
 	log "Launch Parser"
 	$PARSER_PATH --site-config=$SITE_CONF \
+		--source-dir=$CONF_ROOT/workers/spider \
 		--worker-dir=$SITE_ROOT/workers/parser \
 		--shared-dir=$SITE_ROOT/workers/shared \
+		--config-dir=$CONF_DIR/parser \
 		--rule-dir=$SITE_ROOT/rules/parser \
 		--log-file=$SITE_ROOT/logs/parser.log
 
 	log "Launch Reactor"
 	$REACTOR_PATH --site-config=$SITE_CONF \
+		--source-dir=$CONF_ROOT/workers/parser \
 		--worker-dir=$SITE_ROOT/workers/reactor \
 		--shared-dir=$SITE_ROOT/workers/shared \
+		--config-dir=$CONF_DIR/reactor \
 		--rule-dir=$SITE_ROOT/rules/reactor \
 		--log-file=$SITE_ROOT/logs/reactor.log
 
 	log "Launch Assembler"
 	$ASSEMBLER_PATH --site-config=$SITE_CONF \
+		--source-dir=$CONF_ROOT/workers/reactor \
 		--worker-dir=$SITE_ROOT/workers/assembler \
 		--shared-dir=$SITE_ROOT/workers/shared \
+		--webroot-dir=$SITE_ROOT/webroot \
+		--config-dir=$CONF_DIR/assembler \
 		--rule-dir=$SITE_ROOT/rules/assembler \
 		--log-file=$SITE_ROOT/logs/assembler.log
 
