2562b1de12f0f55ea6fbccc4bf5d80604320b897
Cliff Woo <cliffwoo@gmail.com>
2012-07-02 14:39:35 +0800

    Add DNS Record Script

---
+++ src/deployment/addDNSRecord.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from dnspod.apicn import *
+import sys
+def main():
+
+	if len(sys.argv) <> 3:
+		print "You Need Correct Arguments!\nUsage : addDNSRecord record_name ip_address"
+		exit()
+
+	email = '9807068@qq.com'
+	password = '123456'
+	domain_id = '1755356'
+
+
+	api = RecordCreate(sys.argv[1], "A", u'默认'.encode("utf8"), sys.argv[2], 600, domain_id=domain_id,email=email,password=password)
+	record = api().get("record", {})
+	record_id = record.get("id")
+	print "Record id", record_id
+    
+    
+if __name__ == '__main__':
+    main()
