0e9ebeadf4a242a760cd3b54f20aa1f7cdf735fc
zhf <hf@zhanghongfeng.com>
2012-07-11 00:39:11 +0800

    sync src

--- src/deployment/setup/updater/create-updater.sh
+++ src/deployment/setup/updater/create-updater.sh
@@ -5,8 +5,8 @@ SSH_KEY=~/.ssh/esd_rsa
 ADMIN_PUB_KEY=~/.ssh/labrador_rsa.pub
 
 test -n "`getent passwd $USERNAME`" && printf "User already exists.\n" && exit 1
-test -f $SSH_KEY || printf "SSH key $SSH_KEY not found.\n" && exit 1
-test -f $ADMIN_PUB_KEY || printf "SSH public key $ADMIN_PUB_KEY not found.\n" && exit 1
+test ! -f $SSH_KEY && printf "SSH key $SSH_KEY not found.\n" && exit 1
+test ! -f $ADMIN_PUB_KEY && printf "SSH public key $ADMIN_PUB_KEY not found.\n" && exit 1
 
 echo "Adding user updater ..."
 useradd -g users $USERNAME
