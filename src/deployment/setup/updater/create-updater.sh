816bc1066049f4944b8411c60530e256c88be647
zhf <hf@zhanghongfeng.com>
2012-07-11 00:47:12 +0800

    sync src

--- src/deployment/setup/updater/create-updater.sh
+++ src/deployment/setup/updater/create-updater.sh
@@ -9,7 +9,7 @@ test ! -f $SSH_KEY && printf "SSH key $SSH_KEY not found.\n" && exit 1
 test ! -f $ADMIN_PUB_KEY && printf "SSH public key $ADMIN_PUB_KEY not found.\n" && exit 1
 
 echo "Adding user updater ..."
-useradd -g users --home $USERNAME
+useradd -g users --create-home $USERNAME
 
 SSH_DIR=/home/$USERNAME/.ssh
 
