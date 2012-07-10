c9e598c4f1aad19a24e51678f801299becf7af19
zhf <hf@zhanghongfeng.com>
2012-07-11 00:24:09 +0800

    fix detect root bug

--- src/deployment/setup/updater/create-updater.sh
+++ src/deployment/setup/updater/create-updater.sh
@@ -1,4 +1,4 @@
-(( ! $(id -u) = 0 )) && echo "Root only. Switch to root or use 'sudo'." && exit 1
+test `id -u` -ne 0 && echo "Root only. Switch to root or use 'sudo'." && exit 1
 
 USERNAME="updater"
 SSH_KEY=~/.ssh/esd_rsa
