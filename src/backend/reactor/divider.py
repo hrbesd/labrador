3592043fad788b4431ea2f251e00de55b84a91e9
Void Main <voidmain1313113@gmail.com>
2012-07-11 15:10:16 +0800

    finish reactor

--- src/backend/reactor/divider.py
+++ src/backend/reactor/divider.py
@@ -1,4 +1,8 @@
 # -*- encoding:utf-8 -*-
-# 用来处理断句
+# 用来处理断句，并进行请求，将结果存放到服务器
+from tts_client import TTSClient
+
 class Divider:
-	
+	SEPERATOR_CANDIDATE = ['.', '。', ',', '，', '!', '！', '?' , '？', '……']
+
+	def __init__(self):
