7acb22a07736a2b93da9f5484236860296d6e2b7
zhf <hf@zhanghongfeng.com>
2012-07-13 20:54:35 +0800

    The new installer make it even easier to setup

---
+++ src/deployment/setup/tester/installer.sh
@@ -0,0 +1,18 @@
+UPDATER_HOST="219.217.227.82" # You have to verify this!
+
+
+# Check if sudo is available
+sudo -S true < /dev/null &>/dev/null
+
+if test $? -eq 0; then
+	echo "You must be a sudo-er to continue." >&2
+	exit 1
+fi
+
+cd ~
+wget $UPDATER_HOST"/setup.sh"
+chmod +x ~/setup.sh
+echo
+echo
+
+sudo ~/setup.sh --install-dev --check-env
