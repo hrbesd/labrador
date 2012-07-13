3455ee636500184b5e1586700369cd55c6c2ced3
zhf <hf@zhanghongfeng.com>
2012-07-13 21:01:34 +0800

    stupid mistakes make me too old to write code

--- src/deployment/setup/tester/installer.sh
+++ src/deployment/setup/tester/installer.sh
@@ -4,7 +4,7 @@ UPDATER_HOST="219.217.227.82" # You have to verify this!
 # Check if sudo is available
 sudo -S true < /dev/null &>/dev/null
 
-if test $? -eq 0; then
+if test $? -ne 0; then
 	echo "You must be a sudo-er to continue." >&2
 	exit 1
 fi
