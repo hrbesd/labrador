820b3abd68e3078e570c674f801ca0b790a6e899
zhf <hf@zhanghongfeng.com>
2012-07-13 22:06:30 +0800

    re-generate config

--- src/deployment/etc/dir-conf-gen.sh
+++ src/deployment/etc/dir-conf-gen.sh
@@ -1,6 +1,12 @@
+# Known bugs:
+#	* Only relative path works
+
 echo "You must use this script in a repo!"
 read -p "Where is the setup.sh script?" REPLY
-test -f $REPLY || ( echo "File not found."; exit 1)
+if test ! -f "$REPLY"; then
+	printf "File not found:\n$REPLY\n"
+	exit 1
+fi
 
 cat $REPLY | head -50 | tail -39 | sed -e '/^#/d' >directories.conf
 
