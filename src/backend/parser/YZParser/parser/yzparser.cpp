e5b72e71780274d239f11628bc98d479872ee746
hailong <longlongh4@gmail.com>
2012-07-17 13:35:07 +0800

    exit after print --version info

--- src/backend/parser/YZParser/parser/yzparser.cpp
+++ src/backend/parser/YZParser/parser/yzparser.cpp
@@ -15,6 +15,7 @@ YZParser::YZParser(QObject *parent) :
     if(m_paramenters.contains("--version"))
     {
         std::cout<<"Labrador Parser Version "<<BASE_VERSION<<endl;
+        exit(0);
     }
     if(m_paramenters.contains("--log-file"))
     {
