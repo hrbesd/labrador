a3865e8bf6d5edffa2cf2fbf84119bf654078731
Void Main <voidmain1313113@gmail.com>
2012-07-19 07:28:17 +0800

    update multithreaded

--- src/backend/reactor/tts_client.py
+++ src/backend/reactor/tts_client.py
@@ -2,23 +2,17 @@
 # 调用TTS服务，并获取生成的mp3的结果
 import urllib2, base64
 from xml.dom.minidom import parseString
-import profiler
 from threading import Thread, Semaphore
 
-class TTSClient:
-	configDict = {}
+sem = Semaphore(4)
 
-	def __init__(self, configPath):	
-		self.configDict = {}
-		self.sem = Semaphore(4) # limited by tts service
-		configItems = [item for item in open(configPath).readlines()]
-		for item in configItems:
-			key, value = item.split(':', 1)
-			self.configDict[key] = value.strip()
+class TTSWorkerThread(Thread):
+	def __init__(self, text):
+		self.text2send = text
 
-	@profiler.exeTime
-	def sendJobRequest(self, text):
-		self.sem.acquire()
+	def run(self):
+		global sem
+		sem.acquire()
 		conDict = self.configDict
 		urlPath = conDict['jobRequestTemplate'] % (conDict['serverUrl'], conDict['ttsKey'], urllib2.quote(text.encode('utf8')))
 		try:
@@ -27,13 +21,24 @@ class TTSClient:
 		except Exception as e:
 			print 'Error occurred while processing %s' % text
 			print e
-		self.sem.release()
-		return
+
+		sem.release()
+		print 'thread ended'
+
+
+class TTSClient:
+	configDict = {}
+
+	def __init__(self, configPath):	
+		self.configDict = {}
+		configItems = [item for item in open(configPath).readlines()]
+		for item in configItems:
+			key, value = item.split(':', 1)
+			self.configDict[key] = value.strip()
 
 	def generateSound(self, text):
 		if len(text) == 0:
 			return False
 
-		th = Thread(target=self.sendJobRequest, args = (text, ))
+		th = TTSWorkerThread(text)
 		th.start()
-		th.join()
