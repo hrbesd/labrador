f84023d01ee64c1624188c45899e7dc798e6b193
Void Main <voidmain1313113@gmail.com>
2012-07-19 06:58:56 +0800

    add semaphore

--- src/backend/reactor/tts_client.py
+++ src/backend/reactor/tts_client.py
@@ -3,13 +3,14 @@
 import urllib2, base64
 from xml.dom.minidom import parseString
 import profiler
-from threading import Thread
+from threading import Thread, Semaphore
 
 class TTSClient:
 	configDict = {}
 
 	def __init__(self, configPath):	
 		self.configDict = {}
+		self.sem = Semaphore(4) # limited by tts service
 		configItems = [item for item in open(configPath).readlines()]
 		for item in configItems:
 			key, value = item.split(':', 1)
@@ -17,6 +18,7 @@ class TTSClient:
 
 	@profiler.exeTime
 	def sendJobRequest(self, text):
+		self.sem.acquire(True)
 		conDict = self.configDict
 		urlPath = conDict['jobRequestTemplate'] % (conDict['serverUrl'], conDict['ttsKey'], urllib2.quote(text.encode('utf8')))
 		try:
@@ -25,9 +27,11 @@ class TTSClient:
 		except Exception as e:
 			print 'Error occurred while processing %s' % text
 			print e
+		self.sem.release()
 
 	def generateSound(self, text):
 		if len(text) == 0:
 			return False
-		th = Thread(target=sendJobRequest, args = (text))
+
+		th = Thread(target=self.sendJobRequest, args = (text, ))
 		th.start()
