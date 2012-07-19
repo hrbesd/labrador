577e79ffc513a702871b05ac45b6434dfb04d396
Void Main <voidmain1313113@gmail.com>
2012-07-19 08:14:20 +0800

    update

--- src/backend/reactor/tts_client.py
+++ src/backend/reactor/tts_client.py
@@ -20,7 +20,8 @@ class TTSWorkerThread(Thread):
 			conn = urllib2.urlopen(urlPath)
 			conn.close()
 		except Exception as e:
-			print 'Error occurred while processing %s' % self.text2send
+			errorString = u'Error occurred while processing %s' % self.text2send
+			print errorString
 			print e
 
 		sem.release()
