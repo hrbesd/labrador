923a05e827b8c64d2c1c9735ee47599386f35eeb
Void Main <voidmain1313113@gmail.com>
2012-07-22 07:38:22 +0800

    threaded

--- src/backend/reactor/tts_client.py
+++ src/backend/reactor/tts_client.py
@@ -1,20 +1,38 @@
 # -*- encoding:utf-8 -*-
 # 调用TTS服务，并获取生成的mp3的结果
 from suds.client import Client
+from threading import Thread, Semaphore
+
+sem = Semaphore(8)
+
+class ClientThread(Thread):
+	def __init__(self, text, client):
+		self.tts_client = client
+		self.text = text
+
+	def run():
+		global sem
+		self.tts_client.service.text2Speech('zhangjianzong', self.text, 'null')
+		sem.release()
 
 class TTSClient:
 	def __init__(self):	
 		self.tts_client = Client('http://127.0.0.1:7789/SOAP/?wsdl')
 
 	def generateSound(self, text):
+		global sem
+
 		if len(text) == 0:
 			return False
 
 		if text[0] == '&':
 			return False
 
-		result = self.tts_client.service.text2Speech('zhangjianzong', text, 'null')
-		return result
+		sem.acquire()
+		t = ClientThread(text, self.tts_client)
+		t.start()
+		
+		return True
 
 def main():
 	print "Got Here!!!"
