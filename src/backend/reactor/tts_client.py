d14d06c21fbcb0626c7e1db47fdbe2d886b04071
Void Main <voidmain1313113@gmail.com>
2012-07-22 07:28:21 +0800

    update tts proxy with Queue

--- src/backend/reactor/tts_client.py
+++ src/backend/reactor/tts_client.py
@@ -15,3 +15,20 @@ class TTSClient:
 
 		result = self.tts_client.service.text2Speech('zhangjianzong', text, 'null')
 		return result
+
+def main():
+	print "Got Here!!!"
+	t = TTSClient()
+	print "Got Here2!!!!"
+	for i in range(10000):
+		t.generateSound(u'你好中国')
+		print "Processed %d" % i
+
+if __name__ == '__main__':
+	logName = 'tts_client.log'
+	cProfile.run('main()', logName)
+
+	# show it
+	print 'TTS Client result:'
+	p = pstats.Stats(logName)
+	p.strip_dirs().sort_stats('cumulative').print_stats()
