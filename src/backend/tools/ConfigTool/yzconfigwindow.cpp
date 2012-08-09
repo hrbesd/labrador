b4a5cde8e8103cad155c4043da1e9258619fb2b1
hailong <longlongh4@gmail.com>
2012-08-09 16:37:20 +0800

    add actions

--- src/backend/tools/ConfigTool/yzconfigwindow.cpp
+++ src/backend/tools/ConfigTool/yzconfigwindow.cpp
@@ -1,8 +1,12 @@
 #include "yzconfigwindow.h"
+#include <QDebug>
 
 YZConfigWindow::YZConfigWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    createActions();
+    createMenus();
+
     m_spiderConfigWidget=new YZSpiderConfigWidget(this);
     this->setCentralWidget(m_spiderConfigWidget);
     this->resize(800,600);
@@ -11,4 +15,26 @@ YZConfigWindow::YZConfigWindow(QWidget *parent)
 YZConfigWindow::~YZConfigWindow()
 {
     
+}
+
+void YZConfigWindow::createMenus()
+{
+    m_spiderMenu = menuBar()->addMenu(tr("&Spider"));
+    m_spiderMenu->addAction(m_loadAction);
+}
+
+void YZConfigWindow::createActions()
+{
+    m_loadAction = new QAction(tr("Load Config File"),this);
+    connect(m_loadAction,SIGNAL(triggered()),this,SLOT(loadSpiderConfigFile()));
+}
+
+void YZConfigWindow::loadSpiderConfigFile()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+                                                    tr("Open Spider Config File"), "~/", tr("Config Files (*.xml);;Any File(*)"));
+    if(!fileName.isEmpty())
+    {
+        m_spiderConfigWidget->loadSpiderConfigFile(fileName);
+    }
 }
