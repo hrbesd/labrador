08c090f4d67695b42f5eb111f9c922c6b9a45e41
Void Main <voidmain1313113@gmail.com>
2012-08-12 09:53:39 +0800

    integrated index.xsl from outsource

---
+++ src/deployment/etc/templets/stylesheets/outsource/assets/js/accessible/trans.js
@@ -0,0 +1,30 @@
+/**
+ * 语言翻译模块
+ *
+ * 调用Web Service获取某段中文内容的对应翻译
+ * 这里假定原始文字信息是中文的
+ * “包”名： trans
+ *
+ * Author: Void Main
+ */
+
+var trans = {};
+trans.baseURL = "http://localhost:9999/translate/en/";
+
+// 翻译对应的内容
+// 参数：text - 要翻译的文字
+//      callback - 翻译的结果，返回的是字符串内容
+trans.doTranslate = function(text, element, callback) {
+	var translateURL = trans.baseURL + text;
+	$.ajax({
+	    url:translateURL,
+	    type:'GET',
+	    success: function(data)
+	    {
+	        dataJSON = JSON.parse(data);
+	        if(callback) {
+	        	callback(dataJSON.data.translations[0].translatedText, element);
+	        }
+	    }
+	}); 
+}
