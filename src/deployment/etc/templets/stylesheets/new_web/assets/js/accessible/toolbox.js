dbadc5fc42b422ea5d2743ad1dc4b6bc29a29372
hrbesd <hrbesd@gmail.com>
2012-12-28 18:07:53 +0800

    Update src/deployment/etc/templets/stylesheets/new_web/assets/js/accessible/toolbox.js

--- src/deployment/etc/templets/stylesheets/new_web/assets/js/accessible/toolbox.js
+++ src/deployment/etc/templets/stylesheets/new_web/assets/js/accessible/toolbox.js
@@ -150,6 +150,9 @@ var bindActions = function() {
 	//朗读功具栏语音
     $('div[id=toolbar] a').each(function() {
         $(this).bind("mouseenter", function() {
+        	if(speaker!=null && speaker.mp3Object!=null){
+			speaker.mp3Object.destruct();
+		}
 		speaker.toolbar.speak($(this).attr("id"));
         });
         $(this).bind("mouseleave", function() {
