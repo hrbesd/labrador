ddc7222803dd960df1c394a821ca2097d9c893e6
lizhiyong2012 <47443634@qq.com>
2013-03-26 17:18:52 +0800

    Update reactor_main.py

--- src/backend/reactor/reactor_main.py
+++ src/backend/reactor/reactor_main.py
@@ -102,8 +102,7 @@ class Reactor:
 		# &amp;nbsp; => &nbsp; => " "
 		dataContent = html.unescape_string(dataContent)
 		dataContent = dataContent.replace("<o:p>", "<p>").replace("</o:p>", "</p>")
-		dataContent = dataContent.replace("<st1:", "<!--<st1:").replace("st1:chsdate>", "st1:chsdate>-->").replace("st1:chmetcnv>", "st1:chmetcnv>-->") 
-
+		dataContent = dataContent.replace("<st1:", "<!--<st1:").replace("st1:chsdate>", "st1:chsdate>-->").replace("st1:chmetcnv>", "st1:chmetcnv>-->").replace("st1:personname>", "st1:personname>-->")
 
 		parent = codecs.open(parentFile, 'r', 'utf-8')
 		parentContent = parent.read()
