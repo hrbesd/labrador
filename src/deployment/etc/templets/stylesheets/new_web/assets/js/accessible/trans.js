fa7840e554ae38c0cfeb02a6a0bdbe2af5b22f60
张建宗 <zjianzong@gmail.com>
2013-12-30 10:09:53 +0800

    Update trans.js
    翻译更改为百度翻译

--- src/deployment/etc/templets/stylesheets/new_web/assets/js/accessible/trans.js
+++ src/deployment/etc/templets/stylesheets/new_web/assets/js/accessible/trans.js
@@ -10,16 +10,35 @@
 
 var trans = {};
 // VOIDMAIN 修改了对应的TransCache的IP
-trans.baseURL = "http://125.211.222.44:9999/translate/en/";
+//trans.baseURL = "http://125.211.222.44:9999/translate/en/";
 
 // 翻译对应的内容
 // 参数：text - 要翻译的文字
 //      callback - 翻译的结果，返回的是字符串内容
 trans.doTranslate = function(text, callback, element) {
+	var src = text;
+	var b64 = base64.e64(src);
+	url = "http://voice.yunmd.net/baidu-translate-r1.0/translate";
+	$.ajax({
+		url : url,
+		type : 'POST',
+		data : {src:b64},
+		success : function(e) {
+			// alert(e.trans_result[0].dst);
+			callback(0, e.trans_result[0].dst, element);
+		},
+		error : function(jqXHR, textStatus) {
+			callback(1, textStatus, element);
+		},
+		dataType : 'jsonp',
+		async : false
+	});
+	
 	// 发送AJAX请求
 	//var translateURL = trans.baseURL + text;
-	var translateURL = trans.baseURL + encodeURIComponent(text);
-	$.ajax({
+/*
+    var translateURL = trans.baseURL + encodeURIComponent(text);
+    $.ajax({
     url:translateURL,
     dataType: 'jsonp',
     jsonp: false,
@@ -35,4 +54,5 @@ trans.doTranslate = function(text, callback, element) {
     	callback(1, textStatus, element);
     }
 	});
+	*/
 }
